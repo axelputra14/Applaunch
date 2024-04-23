03e084026173389a1dd07e15ef368c309f4b62ae
Axel Putra <octavian.axel.putra@gmail.com>
2024-04-23 16:55:55 +0700

    detail update position color

--- src/views/AppDetail.vue
+++ src/views/AppDetail.vue
@@ -35,38 +35,38 @@ export default {
     class="body bg-contain"
     :style="{ backgroundImage: 'url(' + applistbyid.bgDir + ')' }"
   >
-    <!-- Button Strip Section -->
-    <div class="flex justify-evenly py-4 bg-black bg-opacity-15">
-      <ArrowLeftIcon
-        class="h-12 w-12 text-blue-500 cursor-pointer duration-200 hover:scale-125 active:scale-100"
-        v-on:click="goBack"
-      />
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Play
-      </button>
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Edit
-      </button>
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Delete
-      </button>
+    <div class="bg-gray-800 bg-opacity-35 mx-10 py-10">
+      <!-- Rest of the Content -->
+      <div class="p-8">
+        <!-- Lorem Ipsum paragraphs or any other content -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <!-- Add more paragraphs or content as needed -->
+      </div>
+      <!-- Button Strip Section -->
+      <div class="flex justify-evenly py-4">
+        <ArrowLeftIcon
+          class="h-12 w-12 text-blue-500 cursor-pointer duration-200 hover:scale-125 active:scale-100"
+          v-on:click="goBack"
+        />
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Play
+        </button>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Edit
+        </button>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Delete
+        </button>
+      </div>
     </div>
-
-    <!-- Rest of the Content -->
-    <div class="p-8">
-      <!-- Lorem Ipsum paragraphs or any other content -->
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-      <!-- Add more paragraphs or content as needed -->
-    </div>
-
     <BottomNav />
   </div>
 </template>
