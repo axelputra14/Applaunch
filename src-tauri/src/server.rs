7867ce2b26948096340832198966572c8065ec8e
Axel Putra <octavian.axel.putra@gmail.com>
2024-04-25 13:15:52 +0700

    adding sqlx

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -1,16 +1,21 @@
 use actix_web::{web, App, HttpServer};
+use sqlx::{sqlite::SqlitePool, Sqlite};
 
 use crate::routes;
 
 pub fn start() {
+    // Setup database connection pool
+    let pool = SqlitePool::connect("sqlite:./db/db.sqlite").expect("Failed to create pool");
     // Start Actix web server
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            // Pass the database connection pool to all routes
+            .data(pool.clone())
             // Configure routes
             .service(web::scope("/app").configure(routes::app_routes))
     })
-    .bind("127.0.0.1:16850") // Bind to localhost:8080
+    .bind("127.0.0.1:16850") // Bind to localhost:16850
     .expect("Failed to bind to address")
-    .run();
-    // .expect("Failed to run server");
+    .run()
+    .expect("Failed to run server");
 }
