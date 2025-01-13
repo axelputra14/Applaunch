b9638ff8784b6ce2ad839dc226b77e4e8e6cb129
Axel Putra <octavian.axel.putra@gmail.com>
2025-01-13 16:23:06 +0700

    more migrate to v2 fixes

--- src/apis/applist.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import axiosTauriApiAdapter from "axios-tauri-api-adapter";
-
-const applist = axios.create({
-  adapter: axiosTauriApiAdapter,
-  baseURL: "http://localhost:16850",
-});
-
-export default applist;
--- src/stores/fetch.js
+++ src/stores/fetch.js
@@ -1,6 +1,4 @@
-import { ref, computed } from "vue";
 import { defineStore } from "pinia";
-import applist from "../apis/applist";
 
 import router from "../router";
 
@@ -17,9 +15,16 @@ export const useFetchStore = defineStore({
   actions: {
     async fetchApplists() {
       try {
-        const response = await applist.get("/app");
+        const response = await fetch("http://localhost:16850/app", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-        this.applists = response.data.rows;
+        let data = await response.json();
+
+        this.applists = data.rows;
       } catch (err) {
         this.showAlert = true;
         this.alertMessage = "Failed getting app list";
@@ -27,9 +32,16 @@ export const useFetchStore = defineStore({
     },
     async fetchAppById(id) {
       try {
-        const response = await applist.get(`/app/${id}`);
+        const response = await fetch(`http://localhost:16850/app/${id}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-        this.applistbyid = response.data.result;
+        let data = await response.json();
+
+        this.applistbyid = data.rows;
       } catch (err) {
         this.showAlert = true;
         this.alertMessage = `Failed getting app`;
@@ -37,32 +49,42 @@ export const useFetchStore = defineStore({
     },
     async addApplication(addData) {
       try {
-        console.log("am I here");
-
-        await applist.post("/app/add", {
-          title: addData.title,
-          developer: addData.developer,
-          exeDir: addData.exeDir,
-          imgDir: addData.imgDir,
+        await fetch("http://localhost:16850/app/add", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: addData.title,
+            developer: addData.developer,
+            exeDir: addData.exeDir,
+            imgDir: addData.imgDir,
+          }),
         });
+
         router.push({ name: "applistpage" });
         this.showAlert = true;
         this.alertMessage = "New application has been added";
       } catch (err) {
-        console.log(err);
-
         this.showAlert = true;
         this.alertMessage = "Error in adding data, check some fields";
       }
     },
     async editApplication(editData) {
       try {
-        await applist.patch(`/app/edit/${editData.id}`, {
-          title: editData.title,
-          developer: editData.developer,
-          exeDir: editData.exeDir,
-          imgDir: editData.imgDir,
+        await fetch(`http://localhost:16850/app/edit/${editData.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: editData.title,
+            developer: editData.developer,
+            exeDir: editData.exeDir,
+            imgDir: editData.imgDir,
+          }),
         });
+
         router.push({ name: "applistpage" });
         this.showAlert = true;
         this.alertMessage = "Selected application has been edited";
@@ -74,7 +96,12 @@ export const useFetchStore = defineStore({
     },
     async deleteApplication(id) {
       try {
-        await applist.delete(`/app/${id}`);
+        await fetch(`http://localhost:16850/app/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
         this.showAlert = true;
         this.alertMessage = "Selected application has been deleted";
